b3b755713e29942a32a919b4f19bc639d09fba05
ChristophLasinger <56270498+ChristophLasinger@users.noreply.github.com>
2020-05-28 06:44:04 +0200

    updated coding and css of output, updated css of input and button

--- javascriptFiles/Tutorial3/searchFunction.js
+++ javascriptFiles/Tutorial3/searchFunction.js
@@ -9,6 +9,7 @@ const sunrise = document.querySelector('.sunrise');
 const sunset = document.querySelector('.sunset');
 const button= document.querySelector('.button');
 const apiKey = "119b6b49d1cba0373cac83388df19af2";
+let sunriseHour, sunriseMinute, sunsetHour, sunsetMinute;
 
 button.addEventListener('click', function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid='+apiKey)
@@ -26,6 +27,16 @@ button.addEventListener('click', function(){
       temperatureValue = Math.round(temperatureValue - 273,16);
       sunriseValue = new Date(sunriseValue * 1000);
       sunsetValue = new Date(sunsetValue * 1000);
+      sunriseHour = sunriseValue.getHours();
+      sunriseMinute = sunriseValue.getMinutes();
+      sunsetHour = sunsetValue.getHours();
+      sunsetMinute = sunsetValue.getMinutes();
+      if(sunriseMinute < 10) {
+        sunriseMinute = "0"+sunriseMinute;
+      }
+      if(sunsetMinute < 10) {
+        sunsetMinute = "0"+sunsetMinute;
+      }
 
       name.innerHTML = "Name: "+nameValue;
       country.innerHTML = "Country: "+countryValue;
@@ -33,8 +44,8 @@ button.addEventListener('click', function(){
       humidity.innerHTML = "Humidity: "+humidityValue;
       temperature.innerHTML = "Temperature: ~"+temperatureValue+" °C";
       windspeed.innerHTML = "Windspeed: "+windspeedValue+" km/h";
-      sunrise.innerHTML = "Sunrise: "+sunriseValue.getHours()+":"+sunriseValue.getMinutes()+" Uhr";
-      sunset.innerHTML = "Sunset: "+sunsetValue.getHours()+":"+sunsetValue.getMinutes()+" Uhr";
+      sunrise.innerHTML = "Sunrise: "+sunriseHour+":"+sunriseMinute+" Uhr";
+      sunset.innerHTML = "Sunset: "+sunsetHour+":"+sunsetMinute+" Uhr";
 
       input.value = "";
     })
