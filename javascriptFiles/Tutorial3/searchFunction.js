3803b01198aff5d24ef24d160c1d2deec492e0ad
ChristophLasinger <56270498+ChristophLasinger@users.noreply.github.com>
2020-06-04 09:26:37 +0200

    improved code structure

--- javascriptFiles/Tutorial3/searchFunction.js
+++ javascriptFiles/Tutorial3/searchFunction.js
@@ -1,59 +1,63 @@
 const input = document.querySelector('.input');
-const name = document.querySelector('.name');
-const temperature = document.querySelector('.temperature');
-const description = document.querySelector('.description');
-const country = document.querySelector('.country');
-const humidity = document.querySelector('.humidity');
-const windspeed = document.querySelector('.windspeed');
-const sunrise = document.querySelector('.sunrise');
-const sunset = document.querySelector('.sunset');
 const button= document.querySelector('.button');
-const apiKey = "119b6b49d1cba0373cac83388df19af2";
-let sunriseHour, sunriseMinute, sunsetHour, sunsetMinute;
 
 input.addEventListener('keyup', function(event) {
   if(event.keyCode === 13) {
-    button.click();
+    printAPIData();
   }
 })
+button.addEventListener('click', printAPIData);
 
-button.addEventListener('click', function(){
+
+
+function printAPIData() {
+  const apiKey = "119b6b49d1cba0373cac83388df19af2";
+
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid='+apiKey)
     .then(response => response.json())
     .then(data => {
+      //reading Data from API
       const nameValue = data['name'];
       const descriptionValue = data['weather'][0]['description'];
       const humidityValue = data['main']['humidity'];
       const countryValue = data['sys']['country'];
       const windspeedValue = data['wind']['speed'];
-      let temperatureValue = data['main']['temp'];
-      let sunriseValue = data['sys']['sunrise'];
-      let sunsetValue = data['sys']['sunset'];
+      let temperatureValue = Math.round(data['main']['temp'] - 273,16); //Conversion from Kelvin to Celsius
+      let sunriseValue = new Date(data['sys']['sunrise']*1000); //Conversion from unix time to regular time
+      let sunsetValue = new Date(data['sys']['sunset']*1000); //Conversion from unix time to regular time
 
-      temperatureValue = Math.round(temperatureValue - 273,16);
-      sunriseValue = new Date(sunriseValue * 1000);
-      sunsetValue = new Date(sunsetValue * 1000);
-      sunriseHour = sunriseValue.getHours();
-      sunriseMinute = sunriseValue.getMinutes();
-      sunsetHour = sunsetValue.getHours();
-      sunsetMinute = sunsetValue.getMinutes();
-      if(sunriseMinute < 10) {
-        sunriseMinute = "0"+sunriseMinute;
-      }
-      if(sunsetMinute < 10) {
-        sunsetMinute = "0"+sunsetMinute;
-      }
-
-      name.innerHTML = "Name: "+nameValue;
-      country.innerHTML = "Country: "+countryValue;
-      description.innerHTML = "Description: "+descriptionValue;
-      humidity.innerHTML = "Humidity: "+humidityValue;
-      temperature.innerHTML = "Temperature: ~"+temperatureValue+" °C";
-      windspeed.innerHTML = "Windspeed: "+windspeedValue+" km/h";
-      sunrise.innerHTML = "Sunrise: "+sunriseHour+":"+sunriseMinute+" Uhr";
-      sunset.innerHTML = "Sunset: "+sunsetHour+":"+sunsetMinute+" Uhr";
+      printData(nameValue, countryValue, descriptionValue, humidityValue, temperatureValue, windspeedValue, sunriseValue.getHours(), calculateSunMinute(sunriseValue), sunsetValue.getHours(), calculateSunMinute(sunsetValue));
 
       input.value = "";
     })
     .catch(err => alert("Invalid city name"));
-})
+}
+
+function calculateSunMinute(sunValue) {
+  let result = sunValue.getMinutes();
+
+  if(result < 10){
+    result = "0"+result;
+  }
+  return result;
+}
+
+function printData(nameValue, countryValue, descriptionValue, humidityValue, temperatureValue, windspeedValue, sunriseHour, sunriseMinute, sunsetHour, sunsetMinute) {
+  const name = document.querySelector('.name');
+  const temperature = document.querySelector('.temperature');
+  const description = document.querySelector('.description');
+  const country = document.querySelector('.country');
+  const humidity = document.querySelector('.humidity');
+  const windspeed = document.querySelector('.windspeed');
+  const sunrise = document.querySelector('.sunrise');
+  const sunset = document.querySelector('.sunset');
+
+  name.innerHTML = "Name: "+nameValue;
+  country.innerHTML = "Country: "+countryValue;
+  description.innerHTML = "Description: "+descriptionValue;
+  humidity.innerHTML = "Humidity: "+humidityValue;
+  temperature.innerHTML = "Temperature: ~"+temperatureValue+" °C";
+  windspeed.innerHTML = "Windspeed: "+windspeedValue+" km/h";
+  sunrise.innerHTML = "Sunrise: "+sunriseHour+":"+sunriseMinute+" Uhr";
+  sunset.innerHTML = "Sunset: "+sunsetHour+":"+sunsetMinute+" Uhr";
+}
